71c895e5edfcade4aa895511b5de2118e87f897b
Austin Calvelage <austin.calvelage@icloud.com>
2023-05-07 01:13:56 -0400

    Moved Header into own component, added mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,148 @@
+import cn from 'classnames'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useIsAuthenticated } from 'react-auth-kit'
+
+import NavItem from './NavItem'
+import { MenuToggle } from './Icons'
+import LogoutComponent from './LogoutComponent'
+
+const navLinks = [
+  {
+    page: 'Home',
+    path: '/',
+  },
+  {
+    page: `About`,
+    path: '/about',
+  },
+  {
+    page: 'Contact',
+    path: '/contact',
+  },
+]
 
 const Header = () => {
-  return (
-    <header className='bg-[#222222] opacity-90 p-12'>
-      <div className='flex justify-between items-center'>
-        <img src='/tipseeart.svg' className='w-36' alt='tip see art' />
-        <nav>
-          <ul className='flex gap-3 justify-end'>
-            <li>
-              <Link to={'/'} className='text-white underline text-xl'>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to={'/login'} className='text-white underline text-xl'>
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link to={'/Register'} className='text-white underline text-xl'>
-                Register
-              </Link>
-            </li>
+  const isAuthenticated = useIsAuthenticated()
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-            {
+  /* ↓ Auto Closes Menu when link/button is clicked within ↓ */
+  function mobileMenuHandler() {
+    setMobileMenuOpen((prev) => !prev)
+  }
+
+  function mobileMenuAutoClose() {
+    setMobileMenuOpen((prev) => (prev === true ? false : false))
+  }
+
+  return (
+    <header className='bg-[#222222] p-12 flex justify-between items-center md:items-stretch relative'>
+      <img src='/tipseeart.svg' className='w-36' alt='tip see art' />
+      <nav className='hidden md:flex flex-col justify-between min-h-full max-w-lg w-full'>
+        <ul className='flex gap-5 justify-end items-center'>
+          {isAuthenticated() && (
+            <>
               <li>
-                <Link to={'/profile'} className='text-white underline text-xl'>
+                <LogoutComponent />
+              </li>
+              <li>
+                <Link
+                  className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-white block'
+                  to={'/profile'}
+                >
                   Profile
                 </Link>
               </li>
-            }
+            </>
+          )}
+          {!isAuthenticated() && (
+            <>
+              <li>
+                <Link
+                  className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-white block'
+                  to='/login'
+                >
+                  Log in
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-white block'
+                  to='/signup'
+                >
+                  Sign up
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+        <ul className='flex gap-3 justify-end'>
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavItem path={link.path}>{link.page}</NavItem>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* ↓ Mobile Navbar and toggle button ↓ */}
+
+      <button
+        className='md:hidden relative z-20 w-14 h-14 bg-black rounded-lg text-white flex justify-center items-center'
+        onClick={mobileMenuHandler}
+      >
+        <MenuToggle />
+      </button>
+      <div className={cn('w-full absolute left-0 top-60 z-10 md:hidden h-screen', mobileMenuOpen ? 'block' : 'hidden')}>
+        <nav className='h-1/2 bg-white space-y-10'>
+          <ul className='flex flex-col gap-10 justify-center items-center pt-20'>
+            {navLinks.map((link, index) => (
+              <li key={index} onClick={mobileMenuAutoClose}>
+                <NavItem path={link.path} styles='text-black'>
+                  {link.page}
+                </NavItem>
+              </li>
+            ))}
+          </ul>
+          <ul className='flex justify-center gap-3'>
+            {isAuthenticated() && (
+              <>
+                <li onClick={mobileMenuAutoClose}>
+                  <LogoutComponent />
+                </li>
+                <li onClick={mobileMenuAutoClose}>
+                  <Link
+                    className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-black block'
+                    to={'/profile'}
+                  >
+                    Profile
+                  </Link>
+                </li>
+              </>
+            )}
+            {!isAuthenticated() && (
+              <>
+                <li onClick={mobileMenuAutoClose}>
+                  <Link
+                    className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-black block'
+                    to='/login'
+                  >
+                    Log in
+                  </Link>
+                </li>
+                <li onClick={mobileMenuAutoClose}>
+                  <Link
+                    className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-black block'
+                    to='/signup'
+                  >
+                    Sign up
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
+        <div className='min-h-full bg-gradient-to-b from-white to-white/50' />
       </div>
     </header>
   )
--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -1,42 +1,10 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
 
 const Layout = () => {
   return (
     <>
-      <header className='bg-[#222222] opacity-90 p-12'>
-        <div className='flex justify-between items-center'>
-          <img src='/tipseeart.svg' className='w-36' alt='tip see art' />
-          <nav>
-            <ul className='flex gap-3 justify-end'>
-              <li>
-                <NavLink to={'/'} className='text-white underline text-xl'>
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/login'} className='text-white underline text-xl'>
-                  Login
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/signup'} className='text-white underline text-xl'>
-                  Sign Up
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/profile'} className='text-white underline text-xl'>
-                  Profile
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={'/create-a-profile'} className='text-white underline text-xl'>
-                  Create a Profile
-                </NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
