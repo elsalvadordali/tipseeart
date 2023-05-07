d82f7afe17bfa8e9548553fa21c5c70667536921
Austin Calvelage <austin.calvelage@icloud.com>
2023-05-07 01:07:54 -0400

    Added logout component

---
+++ src/components/LogoutComponent.jsx
@@ -0,0 +1,16 @@
+import { useSignOut } from 'react-auth-kit'
+
+const LogoutComponent = () => {
+  const signOut = useSignOut()
+
+  return (
+    <button
+      className='border-2 border-gray-200 py-1 px-2 outline-none active:border-grey-600 tracking-wider uppercase text-base hover:border-gray-700 text-black md:text-white'
+      onClick={() => signOut()}
+    >
+      Sign Out
+    </button>
+  )
+}
+
+export default LogoutComponent
