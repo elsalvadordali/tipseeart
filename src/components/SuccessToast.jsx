c9cd08a279e53987d23680d104389ba93abc0cdb
Austin Calvelage <austin.calvelage@icloud.com>
2023-05-07 01:09:20 -0400

    Adjusted some styles, added toastType prop.

--- src/components/SuccessToast.jsx
+++ src/components/SuccessToast.jsx
@@ -1,15 +1,14 @@
-const SuccessToast = () => {
+const SuccessToast = ({ toastType }) => {
   return (
-    <div className='h-40 w-52 absolute z-10 flex flex-col items-center justify-center space-y-5 rounded-lg bg-[#222222] opacity-90'>
-      <span className='text-xl text-white'>Registration Complete!</span>
+    <div className='w-full max-w-xs absolute z-10 flex flex-col items-center justify-center space-y-5 rounded-lg bg-[#222222] opacity-90 py-10'>
+      <span className='text-xl text-white'>
+        {toastType === 'register' && 'Registration Complete!'}
+        {toastType === 'login' && 'Login Complete!'}
+      </span>
       <div
         className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-green-400 motion-reduce:animate-[spin_1.5s_linear_infinite]'
         role='status'
-      >
-        <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
-          Loading...
-        </span>
-      </div>
+      ></div>
     </div>
   )
 }
