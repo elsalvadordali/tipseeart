5d312d6976cb2a7bfdf2961bd5b467e15fa3c900
esd <tloochich@icloud.com>
2023-09-07 14:13:00 -0400

    ive given uuuuuuuuuuuuuuuuuuuuuuup

--- firebase.js
+++ firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: 'AIzaSyBFFaPAZ6GSZpEDrFOwa4REICl4otOGJEQ',
+  authDomain: 'artist-profiles-934.firebaseapp.com',
+  projectId: 'artist-profiles-934',
+  storageBucket: 'artist-profiles-934.appspot.com',
+  messagingSenderId: '742036551449',
+  appId: '1:742036551449:web:456802a622a9a368577788',
   storageBucket: 'gs://artist-profiles-934.appspot.com'
 };
 
